2dfa4bba8076153151e87e35689697e11f6d513b
Drazilica <drazic.milan@gmail.com>
2021-03-03 22:17:21 +0100

    router basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from '../src/footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <div>
     <Header />
       <h2>Accounts</h2>
